37d92419ca40c19b77cf432f3ecc39d1690b79eb
Leonardo Tricotti <tricottileo@gmail.com>
2022-08-23 17:42:46 -0300

    transferir casi termonado

--- js/transferir.js
+++ js/transferir.js
@@ -4,6 +4,7 @@ let saldoCajaOperable = localStorage.getItem("saldo");
 const convertirStorageANumero = () => parseFloat(saldoCajaOperable);
 //Variable que almacena el dato convertido a numero
 let saldoOperable = convertirStorageANumero();
+
 const text = document.querySelector(".text");
 text.innerHTML =
   '<p class="text"> Ingrese el monto que desea transferir: <input type="number" class="input" id="transferencia-input"></p>';
@@ -11,8 +12,23 @@ let cuentaSeleccionada = localStorage.getItem("destinatario");
 
 let capturarValor = document.getElementById("transferencia-submit");
 let inputTransferencia = document.getElementById("transferencia-input")
+
+let datoAnumero = parseFloat(inputTransferencia.value);
+
+const operarTransferencia = () => saldoCajaOperable - parseFloat(inputTransferencia.value);
+
+
+
+//Funcion que actualiza el saldo almacenado en el localstorage
+const actualizarSaldoStorage = () =>
+  (saldoCajaAhorro = localStorage.setItem(
+    "saldo",
+    operarTransferencia()
+  ));
+
+
 //Funcion que coinvierte un numero al formato de pesos argentinos
-numeroAPesos = (dinero) => {
+const numeroAPesos = (dinero) => {
   return (dinero = new Intl.NumberFormat("es-AR", {
     style: "currency",
     currency: "ARS",
@@ -21,11 +37,13 @@ numeroAPesos = (dinero) => {
 
 
 
+
+
 //Codigo que dispara un alerta que confirma la operación
 const confirmar = () => {
   Swal.fire({
     icon: "question",
-    title: `Desea transferir a ${cuentaSeleccionada} la suma de `,
+    title: `Desea transferir a ${cuentaSeleccionada} la suma de ${numeroAPesos(operarTransferencia())} `,
     confirmButtonColor: "#3085d6",
     confirmButtonText: "Aceptar",
     showClass: {
@@ -39,14 +57,6 @@ const confirmar = () => {
 let contadorClicks = 0;
 //Funcion que alterna las llamadas a las funciones sobre el mismo boton html
 capturarValor.addEventListener("click", function () {
-  if (contadorClicks == 0) {
-    //Llamada a la funcion que selecciona la cuenta a la cual se desea transferir dinero
-    seleccionarCuenta(inputTransferencia.value);
-    //Codigo que agrega una unidad al contador
-    contadorClicks = 1;
-    console.log(contadorClicks);
-  } else if (contadorClicks == 1) {
-    //Llamada al alert que confirma la operacion
-    confirmar();
-  }
+  confirmar();
+  actualizarSaldoStorage();
 });
